ebbd7d59f1efb41cfef7d129a58e0f0cb6377690
Ralph <98173319+sabioralph@users.noreply.github.com>
2022-03-10 21:40:57 -0500

    Add files via upload

---
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermreviewcodeforpartc;
+import java.util.*;
+
+/**
+ *
+ * @author ralph
+ */
+public class PasswordValidator extends UnoOnline{
+    
+    String password;
+    Boolean validPassword = true;
+    Scanner sc = new Scanner(System.in);
+
+    public void setPassword(String password) {
+        while(!validPassword)
+        {
+            System.out.println("Passwords must have at least 8 characters");
+            System.out.println("Passwords must have at least one special character");
+            System.out.println("Please enter your desired password:"); 
+            
+            password = sc.nextLine();
+            int specialCharCount=0;
+            //iterate over each character to see if it is a special character
+            for(int i=0;i<password.length(); i++)
+            {
+                if(!(Character.isLetterOrDigit(password.charAt(i))))
+                {
+                    //now we know there is at least one special character
+                    specialCharCount++;
+                }
+            }
+            if(specialCharCount>0 &&password.length()>7)
+            {
+                validPassword=true;
+            }
+        this.password = password;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+}
